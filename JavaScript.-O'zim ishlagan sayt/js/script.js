ba64a0d421901d81fa93f9016ed2271b0d0f503c
nurullo0 <n814080@gmail.com>
2022-04-18 18:49:00 +0500

    slider  va class
    slider  yasadik va class yordamida card larni dinamic qoshib chiqdik

--- JavaScript.-O'zim ishlagan sayt/js/script.js
+++ JavaScript.-O'zim ishlagan sayt/js/script.js
@@ -53,7 +53,7 @@ document.addEventListener('DOMContentLoaded', () => {
     modal.classList.remove('hied');
     document.body.style.overflow = 'hidden'
   }
-  
+
   function modalCloses() {
     modal.classList.add('hied');
     modal.classList.remove('show');
@@ -83,7 +83,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function showMyModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
       modalOpening()
-      window.removeEventListener('scroll',showMyModalByScroll)
+      window.removeEventListener('scroll', showMyModalByScroll)
 
     }
   }
@@ -91,16 +91,135 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Modal end
 
+  // class 
+
+  class CarCard {
+    constructor(src, alt, title, descr, price, parentSelectr, ...classess) {
+      this.src = src
+      this.alt = alt
+      this.title = title
+      this.descr = descr
+      this.price = price
+      this.parent = document.querySelector(parentSelectr)
+      this.classess = classess
+      this.tansfer = 10800
+      this.changeToUSD()
+    };
+    changeToUSD() {
+      this.price = this.price * this.tansfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+
+      element.innerHTML = `
+      <div class="menu__item">
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Price:</div>
+          <div class="menu__item-total"><span>${this.price}</span> $</div>
+        </div>
+      </div>
+      `
+      this.parent.append(element);
+    }
+
+  }
+
+  new CarCard(
+    'img/tabs/1.jpg',
+    'Car',
+    '2021 Mercedes-Benz C-Class',
+    ` The 2021 Mercedes-Benz C-Class finishes in the top half of our
+      luxury small car rankings. It's powerful and upscale, but it has
+      so-so handli...`,
+    550,
+    '.menu .container',
+    'red'
+  ).render();
+  new CarCard(
+    'img/tabs/2.jpg',
+    'Car',
+    '2021 Mercedes-Benz C-Class',
+    ` The 2021 Mercedes-Benz C-Class finishes in the top half of our
+      luxury small car rankings. It's powerful and upscale, but it has
+      so-so handli...`,
+    2030,
+    '.menu .container',
+
+  ).render();
+  new CarCard(
+    'img/tabs/3.jpg',
+    'Car',
+    '2021 Mercedes-Benz C-Class',
+    ` The 2021 Mercedes-Benz C-Class finishes in the top half of our
+      luxury small car rankings. It's powerful and upscale, but it has
+      so-so handli...`,
+    500,
+    '.menu .container',
+
+  ).render();
 
 
 
 
 
+  // date vaqt 
+
+  const deadline = '2022-05-01';
+
+  function getTime(endtime) {
+    const total = Date.parse(endtime) - Date.parse(new Date()),
+      days = Math.floor(total / (1000 * 60 * 60 * 24)),
+      hours = Math.floor((total / (1000 * 60 * 60)) % 24),
+      minutes = Math.floor((total / 1000 / 60) % 60)
+    seconds = Math.floor((total / 1000) % 60)
+    return {
+      total: total,
+      days: days,
+      hours: hours,
+      minutes: minutes,
+      seconds: seconds,
+    };
+  }
+
+  function getZero(num) {
+    if (num >= 0 && num < 10) {
+      return '0' + num;
+    } else {
+      return num;
+    }
 
 
+  }
 
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+      days = timer.querySelector('#days'),
+      hours = timer.querySelector('#hours'),
+      minutes = timer.querySelector('#minutes'),
+      seconds = timer.querySelector('#seconds'),
+      timeInterval = setInterval(updateClock, 1000);
 
+    updateClock()
 
+    function updateClock() {
+      const time = getTime(endtime);
+      days.innerHTML = getZero(time.days);
+      hours.innerHTML = getZero(time.hours);
+      minutes.innerHTML = getZero(time.minutes);
+      seconds.innerHTML = getZero(time.seconds);
+      if (time.total <= 0) {
+        clearInterval(timeInterval)
+      }
+    }
+  }
+  setClock(".timer", deadline);
+
+  // slider
 
 
 })
